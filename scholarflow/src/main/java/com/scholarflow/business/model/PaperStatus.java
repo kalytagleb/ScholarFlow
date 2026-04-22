3508c7f6292c21e246de84ee09dfd97124f9a0ad
Gleb <kalytagleb@gmail.com>
2026-04-22 10:10:57 +0200

    Paper model implementation.

---
+++ scholarflow/src/main/java/com/scholarflow/business/model/PaperStatus.java
@@ -0,0 +1,12 @@
+package com.scholarflow.business.model;
+
+public enum PaperStatus {
+    DRAFT,
+    SUBMITTED,
+    UNDER_REVIEW,
+    ACCEPTED,
+    REJECTED,
+    MINOR_REVISION,
+    MAJOR_REVISION,
+    RESUBMITTED
+}
---
+++ scholarflow/src/main/java/com/scholarflow/business/model/state/PaperState.java
@@ -0,0 +1,11 @@
+package com.scholarflow.business.model.state;
+
+import com.scholarflow.business.model.PaperStatus;
+
+/**
+ * Interface, which describes paper behavior in some state.
+ * Every method returns new example of Paper 
+ */
+public interface PaperState {
+    PaperStatus status();
+}
